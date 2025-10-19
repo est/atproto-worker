ccf52e34fb0ca795623469db538bae40c05b3f5d
est <electronixtar@gmail.com>
2025-10-19 11:29:29 +0800

    v1

---
+++ src/index.ts
@@ -0,0 +1,45 @@
+/**
+ * atproto-worker
+ *
+ * - Run `npm run dev` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `npm run deploy` to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+import { ExecutionContext } from '@cloudflare/workers-types';
+
+export interface Env {
+	// Bindings for secrets, KV, etc. will be defined here
+	// e.g. BSKY_USERNAME: string;
+	// e.g. BSKY_PASSWORD: string;
+}
+
+export default {
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		console.log(request.method, request.url);
+
+		// The logic will be structured as follows:
+
+		// 1. Authenticate with bsky.social using secrets
+		// Standalone implementation will go here
+
+		// 2. Handle incoming requests
+		// This could be a webhook from a git provider, or a request from an AppView
+		// const url = new URL(request.url);
+		// if (url.pathname === '/webhook') {
+		//   // Handle git webhook
+		//   // Read data from git repo
+		//   // Post to bsky
+		// }
+
+		// 3. Handle interactions (likes, reposts, etc.)
+		// This would likely involve subscribing to the firehose via a websocket
+		// and filtering for events related to our posts.
+
+		// 4. Commit changes back to git
+		// If we receive interactions, we might want to write them back to our git repo.
+
+		return new Response('Hello from atproto-worker!');
+	},
+};
